399e3d6ec0cad29a56a2327f92758ab30fad3910
Chris Gilmer <chris.gilmer@gmail.com>
2012-02-24 09:04:19 -0800

    Started blog

---
+++ settings.py
@@ -0,0 +1,11 @@
+AUTHOR = 'Chris Gilmer'
+DISQUS_SITENAME = 'chrisgilmerproj'
+GITHUB_URL = 'https://github.com/chrisgilmerproj'
+#GOOGLE_ANALYTICS=''
+SITEURL = 'http://chrisgilmerproj.github.com'
+SITENAME = 'chrisgilmerproj'
+SOCIAL = (('twitter', 'http://twitter.com/chrisgilmerproj'),
+          ('github', 'https://github.com/chrisgilmerproj'),)
+TAG_FEED = 'feeds/%s.atom.xml'
+THEME = 'notmyidea'
+#TWITTER_USERNAME = 'natantus'
